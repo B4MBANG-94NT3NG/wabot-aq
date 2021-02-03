df60e26b4ce0cc90477015e7e042cc284536eed7
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2021-02-03 21:41:24 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • Three Ind [0895412691100]
+│ • Telkomsel [082313021398]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
